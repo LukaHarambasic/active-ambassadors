4cbfb9d92f87a2b2b117c62108fb86d56376a1de
Luka Harambasic <luka@harambasic.de>
2020-04-15 20:36:40 +0200

    black & white - yt redirects

---
+++ pages/Black.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Print on a black shirt</h2>
+    <p>
+      Just wait a few seconds until you get redirected to a YouTube video which
+      will explain you how to iron the transfer paper to your jersey. Click
+      <a
+        href="https://www.youtube.com/watch?v=Xi_2X8sKifY"
+        title="Iron On Transfer Paper For Dark Fabrics Tutorial By Photo Paper Direct"
+        >here</a
+      >
+      if the redirect is not working.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    if (process.client) {
+      // eslint-disable-next-line nuxt/no-globals-in-created
+      window.location.replace('https://www.youtube.com/watch?v=Xi_2X8sKifY')
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped></style>
---
+++ pages/White.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Print on a white shirt</h2>
+    <p>
+      Just wait a few seconds until you get redirected to a YouTube video which
+      will explain you how to iron the transfer paper to your jersey. Click
+      <a
+        href="https://www.youtube.com/watch?v=AXCh6rW2LbY"
+        title="How To Use T Shirt Transfer Paper Step By Step"
+        >here</a
+      >
+      if the redirect is not working.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    if (process.client) {
+      // eslint-disable-next-line nuxt/no-globals-in-created
+      window.location.replace('https://www.youtube.com/watch?v=AXCh6rW2LbY')
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped></style>

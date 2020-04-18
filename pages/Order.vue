6115122364d3d06772bc587309f57f82e6291e64
Luka Harambasic <luka@harambasic.de>
2020-04-18 22:20:51 +0200

    Dev (#9)
    * Responsive Version (#2) (#3)
    
    * mobile layout
    
    * Adjusted heading for exemplary picture
    
    * image optimization & mobile optimization
    
    * refactoring in components, added vuex
    
    * working in modal
    
    * fixed modal and added helper functions to store
    
    * refactoring
    
    * logo fix
    
    * hero fix
    
    * new favicon
    
    * footer center fix
    
    * rework: icons, esc to close
    
    * local fonts and image optimization and privacy
    
    * removed data page
    
    * added hostname to sitemap config
    
    * fixed error in footer -> link spacing
    
    * added lang & label for a18n
    
    * removed label
    
    * fix package-lock
    
    * removed empty page
    
    * black & white - yt redirects
    
    * transparency expense sheet & order page
    
    Co-authored-by: Leonard Schwier <github@leonardschwier.de>

---
+++ pages/Order.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Order your jersey now</h2>
+    <p>
+      Just wait a few seconds until you get redirected to a Google. Click
+      <a
+        href="https://forms.gle/T5Pfk9XXF3tG685cA"
+        title="Order your jersey now"
+        >here</a
+      >
+      if the redirect is not working.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    if (process.client) {
+      // eslint-disable-next-line nuxt/no-globals-in-created
+      window.location.replace('https://forms.gle/T5Pfk9XXF3tG685cA')
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped></style>
--- pages/Transparency.vue
+++ pages/Transparency.vue
@@ -1,5 +1,13 @@
 <template>
-  <h2>Transparency</h2>
+  <div>
+    <h2>Transparency</h2>
+    <p>
+      <a
+        href="https://docs.google.com/spreadsheets/d/1IQuvNWEWRC5HMRhYhcOUJRylo6w5Js3lBl6lw_DgEiI/edit?usp=sharing"
+        >Expenses</a
+      >
+    </p>
+  </div>
 </template>
 
 <script>

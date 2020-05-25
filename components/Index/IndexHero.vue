f827b192609a743b1ed89261baaf14a31064850a
Leonard Schwier <github@leonardschwier.de>
2020-05-25 11:59:19 +0200

    Update IndexHero.vue

--- components/Index/IndexHero.vue
+++ components/Index/IndexHero.vue
@@ -9,21 +9,19 @@
       We bring athletes and NGOs together. Brand your jersey now.
     </blockquote>
   </section>
-</template>
-<template>
   <section>
     We will send you all what you need so you can brand your favorite jersey at home.<br />
     <p>Your favorite jersey + our do-it-yourself (DIY) kit</p>
     <br/><br/>
-    <p>[1]<br/>
-    Please fill in our form mentioning your address, contact details and the organisation you want to support
+    <p><br/>[1]<br/>
+    Please fill in our form (<a href="https://docs.google.com/forms/d/e/1FAIpQLSc62vEnwDXqLOKlADTAMz9cUNU3eEltvcT6-aOTRmQcHQu_2w/viewform?usp=sf_link">click here</a>) mentioning your address, contact details and the organisation you want to support
     <hr/>
     </p>
-    <p>[2]<br/>
+    <p><br/>[2]<br/>
     We send you a parcel with a do-it yourself (DIY) kit so you can brand your own jersey at home. It is a super easy process, you just need an iron - everything else will be sent by us.
     <hr/>
     </p>
-    <p>[3]<br/>
+    <p><br/>[3]<br/>
     Share you experience with us and your friends on social media!<hr/>
     </p>
   </section>

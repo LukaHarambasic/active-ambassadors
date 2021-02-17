0e2c106d2a39266d1d9dedc0490c97dab2b04feb
Luka Harambasic <luka@harambasic.de>
2021-02-17 15:28:19 +0100

    updating team page

--- pages/About-Us.vue
+++ pages/About-Us.vue
@@ -12,7 +12,17 @@
         <ul class="team">
           <li v-for="(member, index) in team" :key="index">
             <img :src="member.picture" :alt="getAlt(member.name)" />
-            <h4 class="title" v-text="member.name" />
+            <div class="title-row">
+              <h4 class="title" v-text="member.name" />
+              <ul class="contact">
+                <li>
+                  <a :href="member.linkedin"><icon-linkedin /></a>
+                </li>
+                <li>
+                  <a :href="getMailto(member.email)"><icon-email /></a>
+                </li>
+              </ul>
+            </div>
             <span class="position" v-text="member.position" />
             <ul class="sport">
               <li
@@ -20,14 +30,6 @@
                 :key="indexSport"
               >
                 <span v-text="emoji" />
-              </li>
-            </ul>
-            <ul class="contact">
-              <li>
-                <a :href="member.linkedin"><icon-linkedin /></a>
-              </li>
-              <li>
-                <a :href="getMailto(member.email)"><icon-email /></a>
               </li>
             </ul>
           </li>
@@ -56,22 +58,22 @@ export default {
         {
           picture: '/active-ambassadors/active-ambassador-julia.jpg',
           name: 'Julia',
-          position: 'Marketing',
+          position: 'Marketing & Podcast',
           linkedin: 'https://www.linkedin.com/in/julia-ha%C3%9Ffeld/',
           email: 'julia@active-ambassadors.org',
           sports: ['🏃🏼‍', '🧘🏼']
         },
         {
-          picture: '/active-ambassadors/active-ambassador-julia.jpg',
+          picture: '/active-ambassadors/active-ambassador-julia-mt.png',
           name: 'Julia',
-          position: 'Lorem Ipsum',
+          position: 'Digital Creation & Podcast',
           linkedin:
             'https://www.linkedin.com/in/julia-margeth-theuer-she-her-30619210b/',
           email: 'julia.mt@active-ambassadors.org',
-          sports: ['🏃🏼‍', '🧘🏼']
+          sports: ['🧗‍‍', '🚴', '💃']
         },
         {
-          picture: '/active-ambassadors/active-ambassador-leo.jpg',
+          picture: '/active-ambassadors/active-ambassador-luka.jpg',
           name: 'Luka',
           position: 'Website, Tools & Automation',
           linkedin: 'https://www.linkedin.com/in/harambasic',
@@ -100,7 +102,7 @@ export default {
   justify-content: space-between
   align-content: stretch
   align-items: stretch
-  li
+  > li
     width: calc((100% / 3) - ((2 * 2rem) / 3)) // 100% / n - ((n-1) * spacing) / n
     border-radius: $border-radius
     background: $color-primary-light
@@ -118,15 +120,54 @@ export default {
       width: 100%
 img
   margin: 0 0 1rem 0
+  border-radius: $border-radius
+.title-row
+  display: flex
+  flex-direction: row
+  flex-wrap: nowrap
+  justify-content: flex-start
+  align-content: flex-start
+  align-items: center
 .title
   font-size: 1.5rem
+  line-height: 1
+.contact
+  list-style: none
+  padding: 0
+  display: flex
+  flex-direction: row
+  flex-wrap: nowrap
+  justify-content: flex-start
+  align-content: center
+  align-items: center
+  li
+    font-size: 1.2rem
+    margin: 0 0 0 .5rem
+    text-align: center
+    display: flex
+    flex-direction: row
+    flex-wrap: nowrap
+    justify-content: center
+    align-content: center
+    align-items: center
+    a
+      border: none
+      display: inline
+      &:hover
+        border: none
+        transform: scale(1.2)
+    svg
+      background: $color-light
+      height: 1rem
+      transition: $animation
+      fill: $color-secondary
+      border-radius: 2px
 .position
   margin: 0 0 .5rem 0
   text-align: center
 .sport
   list-style: none
   padding: 0
-  margin: 0 0 .5rem 0
   display: flex
   flex-direction: row
   flex-wrap: nowrap
@@ -148,41 +189,4 @@ img
     align-content: center
     align-items: center
     margin: 0 .5rem 0 0
-.contact
-  list-style: none
-  padding: 0
-  display: flex
-  flex-direction: row
-  flex-wrap: nowrap
-  justify-content: flex-start
-  align-content: stretch
-  align-items: stretch
-  li
-    background: $color-light
-    width: 2rem
-    height: 2rem
-    font-size: 1.2rem
-    padding: .5rem
-    border-radius: 100%
-    text-align: center
-    display: flex
-    flex-direction: row
-    flex-wrap: nowrap
-    justify-content: center
-    align-content: center
-    align-items: center
-    margin: 0 .5rem 0 0
-    transition: $animation
-    &:hover
-      background: $color-secondary
-      svg
-        fill: $color-light
-    a
-      &:hover
-        border: none
-    svg
-      width: 100%
-      height: 100%
-      transition: $animation
-      fill: $color-secondary
 </style>
